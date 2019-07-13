16c28b2caca4fd893e537530b571926c96577d95
Taylor <tbuchhei@gmail.com>
2019-07-12 20:58:32 -0700

    Start to animation system

---
+++ Shenanijam2019/Animation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shenanijam2019
+{
+    class Animation
+    {
+        public int currFrame { get; set; }
+        public int totalFrames { get; set; }
+        public int spriteHeight { get; set; }
+        public int spriteWidth { get; set; }
+        public Texture2D spriteSheet { get; set; }
+
+        public Animation(int totalFrames, int spriteHeight, int spriteWidth, Texture2D spriteSheet)
+        {
+            currFrame = 0;
+            this.totalFrames = totalFrames;
+            this.spriteHeight = spriteHeight;
+            this.spriteWidth = spriteWidth;
+        }
+
+        public void Update()
+        {
+            if(currFrame < totalFrames)
+            {
+                currFrame++;
+            }
+            else
+            {
+                currFrame = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            Rectangle source = new Rectangle(currFrame * spriteWidth, spriteHeight, spriteWidth, spriteHeight);
+
+            sb.Begin();
+
+            sb.End();
+        }
+    }
+}
---
+++ Shenanijam2019/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shenanijam2019
+{
+    class Player
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Name { get; set; }
+        public Dictionary<string, Animation> Animations;
+
+        public Player()
+        {
+            Animations = new Dictionary<string, Animation>();
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Begin();
+
+            sb.End();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="animation"></param>
+        public void AddAnimation(string name, Animation animation)
+        {
+            Animations.Add(name, animation);
+        }
+    }
+}
